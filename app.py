0687d5f5835d31921ed4eb98ca70e423a5bea771
Semira Fernando <67985897+semiraf@users.noreply.github.com>
2023-07-14 13:48:11 +1000

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ unit = input('(L)bs or (K)g: ')
 
 if unit.upper() == "K":
     converted = weight * 0.45
-    print(converted)
+    print(f"You are: {converted}")
 
 elif unit.upper() == "L":
     converted = weight / 0.45
-    print(converted)
+    print(f"You are: {converted}")
