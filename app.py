1060a6046bd2b5b89bc3ba1b5ffd8040e60d2e5e
Semira Fernando <67985897+semiraf@users.noreply.github.com>
2023-07-14 14:03:56 +1000

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ guess_limit = 3
 
 while guess_count < guess_limit:
     user_guess = int(input('Guess: '))
+    guess_count += 1
 
     if user_guess == secret_number:
         print("You win!")
@@ -14,3 +15,6 @@ while guess_count < guess_limit:
 
     elif user_guess >= secret_number:
         print("Lower!")
+        
+    elif guess_limit == guess_limit:
+        print('You lose!')
