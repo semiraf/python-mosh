53bb11b2748276d2f177c1fafd5a7a9e72ace62d
Semira Fernando <67985897+semiraf@users.noreply.github.com>
2023-07-14 12:49:08 +1000

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ if good_credit == True:
     down_payment = 0.1 * price
     
 elif good_credit == False:
-    house_price = 0.2 * price
+    down_payment = 0.2 * price
 
 print(f"Down payment: {house_price}")
