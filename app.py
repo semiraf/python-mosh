98e99fca330488f361758c29aab6db300aff5d9e
Semira Fernando <67985897+semiraf@users.noreply.github.com>
2023-07-14 14:01:36 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import random
 secret_number = (random.randint(1, 10))
+guess_count = 0
+guess_limit = 3
 
-while:
+while guess_count < guess_limit:
     user_guess = int(input('Guess: '))
 
     if user_guess == secret_number:
