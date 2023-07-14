ed12f47a2630451f0e5071c4c62ce614144a7ee5
Semira Fernando <67985897+semiraf@users.noreply.github.com>
2023-07-14 12:44:50 +1000

    Create python.py

---
+++ python.py
@@ -0,0 +1,10 @@
+house_price = 1000000
+good_credit = False
+
+if good_credit == True:
+    house_price = 1000000 - (1000000 / 10)
+    
+elif good_credit == False:
+    house_price = 1000000 - (1000000 / 20)
+
+print(house_price)
